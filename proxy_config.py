a5680e6a0e021cb176c1bcfff5ff8f1911883009
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-08-17 09:46:47 +0700

    Update proxy_config.py

--- proxy_config.py
+++ proxy_config.py
@@ -1,5 +1,5 @@
 try:
-    with open(r'proxies.txt', 'r') as file:
+    with open(r'/config/proxies.txt', 'r') as file:
         proxy = [line.rstrip() for line in file.readlines()]
 except FileNotFoundError:
     raise Exception('proxies.txt tidak ada.')
